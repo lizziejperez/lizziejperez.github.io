a5fc14ad75fb6303e82e49b8df8efcda0d28c4f4
Lizzie Perez <lizziejperez@gmail.com>
2022-07-05 14:33:16 -0400

    Create footer.js

---
+++ scripts/footer.js
@@ -0,0 +1,17 @@
+// Footer Content Population
+var contactHTML = '<div class="contact"><h3><a href="mailto:lizziejperez@gmail.com" class="h-link">lizziejperez@gmail.com</a></h3></div>';
+var linksHTML = '<div class="links">';
+var links = [
+    ['https://www.linkedin.com/in/elizabeth-perez-0b22b6109', 'LinkedIn'],
+    ['https://github.com/lizziejperez/', 'GitHub'],
+    ['https://lizziejperez.itch.io/', 'Itch.io'],
+    // ['https://twitter.com/nirdypanda', 'Twitter'],
+    // ['https://www.instagram.com/nirdypanda/', 'Instagram'],
+    ['https://www.youtube.com/channel/UC3eF1VaU20H_IWCSBttt6Qg', 'YouTube'],
+    ['https://soundcloud.com/user-280111311', 'SoundCloud']
+];
+for(let i = 0; i < links.length; i++) {
+    linksHTML += '<a href="' + links[i][0] + '" target="_blank">' + links[i][1] + '</a>';
+}
+linksHTML += '</div>';
+document.body.innerHTML += '<footer>' + linksHTML + contactHTML + '</footer>';
